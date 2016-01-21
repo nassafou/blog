60bdb9ded0df7b9367655755882683d2604edebc
nassafou <ntyoussouf@gmail.com>
2016-01-21 14:26:12 -0800

    article

---
+++ src/Sdz/BlogBundle/DataFixtures/ORM/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sdz\BlogBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sdz\BlogBundle\Entity\Competence;
+
+class Article implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        //Liste des articles
+        
+    }
+}
